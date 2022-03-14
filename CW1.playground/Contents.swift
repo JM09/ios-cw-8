19b8cb400dc333dbf6a0754f80c4fb6c0e946b48
JM09 <jassimbackup62@gmail.com>
2022-03-14 19:26:01 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,21 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
+var myName = "Jassem"
+var mybirthYear = 2005
+var myAge = 2022 - mybirthYear
+
+
+
+print("Hello \(myName) is your name and \(myAge) is your age")
+
+var Stringnumber = "8"
+var num1 = 4
+var doublenumber = 2.5
+
+var number = num1 + Int(Stringnumber)!
+
+
+
+
 
